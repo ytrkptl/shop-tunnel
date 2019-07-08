692382170fe8d337f2bc681641f6751010ce3752
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2019-07-08 08:20:09 -0400

    fixed error in user selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
+import { createStructuredSelector } from 'reselect';
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({ cartItems }) => (
@@ -16,8 +17,8 @@ const CartDropdown = ({ cartItems }) => (
 	</div>
 );
 
-const mapStateToProps = state => ({
-	cartItems: selectCartItems(state)
+const mapStateToProps = createStructuredSelector({
+	cartItems: selectCartItems
 });
 
 export default connect(mapStateToProps)(CartDropdown);
--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
 import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+import { createStructuredSelector } from 'reselect';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import './cart-icon.styles.scss';
 
@@ -12,11 +13,12 @@ const CartIcon = ({ toggleCartHidden, itemCount }) => (
 	</div>
 );
 
-const mapStateToProps = state => ({
-	itemCount: selectCartItemsCount(state)
-});
-
 const mapDispatchToProps = dispatch => ({
 	toggleCartHidden: () => dispatch(toggleCartHidden())
 });
+
+const mapStateToProps = createStructuredSelector({
+	itemCount: selectCartItemsCount
+});
+
 export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
