751bf7b497f249db5a3572f00561c63d9dd601df
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2019-07-08 08:49:59 -0400

    added checkout page, and total cost is displaying and working on that page when adding items

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,16 +4,22 @@ import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { selectCartItems } from '../../redux/cart/cart.selectors';
 import { createStructuredSelector } from 'reselect';
+import { withRouter } from 'react-router-dom';
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = ({ cartItems }) => (
+const CartDropdown = ({ cartItems, history }) => (
 	<div className='cart-dropdown'>
 		<div className='cart-items'>
 			{
-				cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+				cartItems.length?
+				cartItems.map(cartItem => (
+					<CartItem key={cartItem.id} item={cartItem} />
+				))
+				:
+				(<span className='empty-message'>Your cart is empty</span>)
 			}
 		</div>
-		<CustomButton>GO TO CHECKOUT</CustomButton>
+		<CustomButton onClick={()=> history.push('/checkout')}>GO TO CHECKOUT</CustomButton>
 	</div>
 );
 
@@ -21,4 +27,4 @@ const mapStateToProps = createStructuredSelector({
 	cartItems: selectCartItems
 });
 
-export default connect(mapStateToProps)(CartDropdown);
+export default withRouter(connect(mapStateToProps)(CartDropdown));
