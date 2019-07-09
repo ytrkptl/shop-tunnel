29973c8b538c939b67e9b8a6865d9cd0d72d17b7
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2019-07-09 10:08:26 -0400

    moved directory and shop data into its own reducer, and created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,8 @@ const config = {
     appId: "1:838632403416:web:e1be8013d37cf990"
   };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
 	if(!userAuth) return;
 	const userRef = firestore.doc(`users/${userAuth.uid}`);
@@ -33,8 +35,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 	}
 	return userRef;
 }
-
-firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
