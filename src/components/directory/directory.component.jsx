2091150c0301d8fde8641802e1bc4c5f5c4b9b5b
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2019-07-24 18:24:30 -0400

    converted more components to use styled components including: form-input, directory, collections-overview, collection-preview, etc.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
-import './directory.styles.scss';
-import MenuItem from '../menu-item/menu-item.component';
 import { connect } from 'react-redux';
-import { selectDirectorySections } from '../../redux/directory/directory.selectors';
 import { createStructuredSelector } from 'reselect';
 
+import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+
+import MenuItem from '../menu-item/menu-item.component';
+
+import { DirectoryMenuContainer } from './directory.styles';
+
 const Directory = ({ sections }) => (
-	<div className='directory-menu'>
-		{
-			sections.map(({id, ...otherSectionProps}) => (
-				<MenuItem key={id} {...otherSectionProps}/>
-			))}
-	</div>
+  <DirectoryMenuContainer>
+    {sections.map(({ id, ...otherSectionProps }) => (
+      <MenuItem key={id} {...otherSectionProps} />
+    ))}
+  </DirectoryMenuContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
