9c224179fbf98b045e21732f44e6588e48fbccb8
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2019-09-11 18:50:06 -0400

    added Error boundaries, lazy, and suspense

--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -1,18 +1,12 @@
 import React from 'react';
+import Spinner from '../spinner/spinner.component';
 
-import { SpinnerContainer, SpinnerOverlay } from './with-spinner.styles';
-
-const WithSpinner = WrappedComponent => {
-  const Spinner = ({ isLoading, ...otherProps }) => {
+const WithSpinner = WrappedComponent => ({ isLoading, ...otherProps }) => {
   return isLoading ? (
-    <SpinnerOverlay>
-      <SpinnerContainer />
-    </SpinnerOverlay>
+    <Spinner/>
   ) : (
     <WrappedComponent {...otherProps} />
   );
-}
-  return Spinner;
 }
 
 export default WithSpinner;
