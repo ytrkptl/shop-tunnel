fa060a394d0f5b326356a27d058e3be7a909499c
ytrkptl <35569392+ytrkptl@users.noreply.github.com>
2020-05-13 20:31:24 -0400

    removed bodyParser and replaced with express.json

--- server.js
+++ server.js
@@ -1,58 +1,57 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
-const enforce = require('express-sslify');
-const compression = require('compression');
-const sendContactForm = require('./controllers/send-contact-form.js');
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const enforce = require("express-sslify");
+const compression = require("compression");
+const sendContactForm = require("./controllers/send-contact-form.js");
 
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors());
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   app.use(compression());
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
-app.listen(port, error => {
+app.listen(port, (error) => {
   if (error) throw error;
-  console.log('Server is running on port ' + port);
+  console.log("Server is running on port " + port);
 });
 
-app.get('/service-worker.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
-})
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
+});
 
-app.post('/payment', (req, res) => {
+app.post("/payment", (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
-    currency: 'usd'
+    currency: "usd",
   };
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
     if (stripeErr) {
-      res.status(500).send({ error: stripeErr })
+      res.status(500).send({ error: stripeErr });
     } else {
       res.status(200).send({ success: stripeRes });
     }
-  })
-})
+  });
+});
 
-app.post('/send-contact-form', (req, res) => {
-  sendContactForm.handleSendingContactForm(req, res)
-})
+app.post("/send-contact-form", (req, res) => {
+  sendContactForm.handleSendingContactForm(req, res);
+});
